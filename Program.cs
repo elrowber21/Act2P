c556c0cc726a894d6be53f823b4588d46df7832e
elrowber21 <60397994+elrowber21@users.noreply.github.com>
2020-02-21 13:58:48 -0800

    V1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Peliculas
+{   
+    
+    class Peliculas
+    { 
+    public string Titulo, Pais, Director;
+    public int Año;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Peliculas Pelicula1= new Peliculas();
+           Peliculas Pelicula2= new Peliculas();
+
+           Pelicula1.Titulo= "Spider-Man 2";
+           Pelicula2.Titulo="Joker";
+           Pelicula1.Año=2004;
+           Pelicula2.Año=2019;
+           Pelicula1.Pais="USA";
+           Pelicula2.Pais="USA";
+           Pelicula1.Director="Saim Raimi";
+           Pelicula2.Director="Todd Phillips";
+
+           Console.WriteLine(Pelicula1.Titulo);
+           Console.WriteLine(Pelicula1.Año);
+           Console.WriteLine(Pelicula2.Titulo);
+           Console.WriteLine(Pelicula2.Año);
+           
+        }
+    }
+}
